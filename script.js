b8e317affa6a989fbf969b0b4adcce24c3dc7d95
Matija0408 <msekelj@gmail.com>
2021-07-26 22:48:33 +0100

    testing arrow functions

--- script.js
+++ script.js
@@ -1,2 +1,20 @@
-let email = document.getElementById("emailInput").Value;
-console.log(email);
+//variable to hold the input value
+
+//variable to validate the input value against
+
+//function to do the input check against set parameters
+function ValidateEmail(inputText) {
+  var mailformat = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3})+$/;
+  if (inputText.value.match(mailformat)) {
+    alert("You have entered a valid email address!"); //The pop up alert for a valid email address
+    document.form1.text1.focus();
+    return true;
+  } else {
+    alert("You have entered an invalid email address!"); //The pop up alert for an invalid email address
+    document.form1.text1.focus();
+    return false;
+  }
+}
+// TESTING ARROW FUNCTIONS
+let sum = (a, b) => a + b;
+console.log(sum(2, 3));

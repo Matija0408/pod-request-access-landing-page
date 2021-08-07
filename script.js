6ee009cd8b56d97e8cdb1cdea2cf0174fdb3cdf4
Matija0408 <msekelj@gmail.com>
2021-08-07 18:09:05 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-//variable to hold the input value
-
-//variable to validate the input value against
-
-//function to do the input check against set parameters
 document.getElementById("btn").addEventListener("click", function (event) {
   event.preventDefault();
 });
@@ -22,23 +17,5 @@ function validateEmail() {
   } else {
     return (document.getElementById("emailFieldNotification").innerHTML =
       "Please enter a valid email address!");
-  }
-}
-
-// Use this >>>> /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-
-function validate() {
-  let mail = document.getElementById("email").value;
-
-  let pattern = /^([a-zA-Z0-9\._]+)@([a-zA-Z0-9])+.([a-z]+)(.[a-z]+)?$/;
-
-  if (pattern.test(mail)) {
-    let emailNote = (document.getElementById("emailNote").innerHTML =
-      "Email valid");
-    return true;
-  } else {
-    let emailNote = (document.getElementById("emailNote").innerHTML =
-      "Please enter valid email address");
-    return false;
   }
 }
